1621df8a00911072a41cd9503bd75a75139744da
mihirkelkar <kelkarmhr@gmail.com>
2014-06-26 21:01:45 -0700

    Adding some more part of the app

--- app/src/main/java/com/mihir/tsearch/app/MainActivity.java
+++ app/src/main/java/com/mihir/tsearch/app/MainActivity.java
@@ -1,17 +1,72 @@
 package com.mihir.tsearch.app;
 
+
+import java.util.Arrays;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
 
 
 public class MainActivity extends ActionBarActivity {
+    private SharedPreferences savedSearches;
+    private TableLayout queryTableLayout;
+    private EditText queryEditText;
+    private EditText tagEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //get the shared preferences that contains the user's saved searches.
+        savedSearches = getSharedPreferences("searches", MODE_PRIVATE);
+
+        //get a reference to the queryTable
+        queryTableLayout = (TableLayout) findViewById(R.id.queryTableLayout);
+        queryEditText = (EditText) findViewById(R.id.queryEditText);
+        tagEditText = (EditText) findViewById(R.id.tagEditText);
+
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(saveButtonListener);
+
+        Button clearTagsButton = (Button) findViewById(R.id.clearTagsButton);
+        clearTagsButton.setOnClickListener(clearTagsButtonListener);
+
+        refreshButtons(null);
+
+    }
+
+    private void refreshButtons(String newTag){
+        String[] tags = savedSearches.getAll().keySet().toArray(new String[0]);
+        Arrays.sort(tags, String.CASE_INSENSITIVE_ORDER);
+
+        if(newTag != null)
+        {
+            makeTagGUI(newTag, Arrays.binarySearch(tags, newTag));
+        }
+        else
+        {
+            for(int index = 0; index < tags.length; ++index){
+                makeTagGUI(tags[index], index);
+            }
+        }
+
+
     }
 
 
